5ff91a9b6fbeb9c093f44caf853cd876e55535b5
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-25 01:21:44 +0300

    add routes for home in app.jsx and its imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
-function App() {
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 
+export default function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Router>
+      <div className="min-h-screen bg-background text-foreground">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
-export default App;
