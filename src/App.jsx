cd93d4d56949cf227d5a28dc5bd15610d70d2a81
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-25 11:22:22 +0300

    add footer into main app router

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import Home from './pages/Home';
 
 export default function App() {
@@ -14,14 +15,20 @@ export default function App() {
 
   return (
     <Router>
+     
       <div className="min-h-screen bg-background text-foreground flex flex-col transition-colors duration-200">
         
+       
         <Navbar darkMode={darkMode} setDarkMode={setDarkMode} />
+        
+       
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
           </Routes>
         </main>
+  
+        <Footer />
         
       </div>
     </Router>
