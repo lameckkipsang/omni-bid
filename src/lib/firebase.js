19835de2b905e8d5202d1b006825353024fbd366
Lameck Kipsang <lameck.ky@gmail.com>
2026-08-28 23:26:40 +0300

    update firebase.js by initializing and exporting firebase storage instance

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -20,3 +21,4 @@ export const googleProvider = new GoogleAuthProvider();
 
 // Initialize Cloud Firestore and get a reference to the service
 export const db = getFirestore(app);
+export const storage = getStorage(app);
